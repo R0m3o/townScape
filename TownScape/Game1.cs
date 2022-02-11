1eea52606bfa9e73fd2915f4af36a024dfad2e35
R0m3o <mysterion.i.am.no1@gmail.com>
2022-02-11 08:10:45 +0100

    the event doesn't fully work but some of the code is written

--- TownScape/Game1.cs
+++ TownScape/Game1.cs
@@ -20,6 +20,8 @@ namespace TownScape
         //Make it so that we can use a font
         private SpriteFont font;
 
+        private Texture2D sprite;
+        private Rectangle rectangle;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -42,6 +44,10 @@ namespace TownScape
 
             //set our customized font as the game font
             font = Content.Load<SpriteFont>("TextTownScape");
+
+            sprite = Content.Load<Texture2D>("button-gf531801aa_640");
+            //makes a rectangle
+            rectangle = new Rectangle(200, 230, 200, 60);
         }
 
         protected override void Update(GameTime gameTime)
@@ -50,6 +56,8 @@ namespace TownScape
                 Exit();
 
             // TODO: Add your update logic here
+            
+
             if (sawmill.WoodPoints <= 9999)
             {
                 sawmill.TheSawmill();
@@ -87,10 +95,33 @@ namespace TownScape
 
             _spriteBatch.DrawString(font, "Manpower: " + manpowerPoints, new Vector2(375, 450), Color.Black);
 
+            //mine
+            _spriteBatch.DrawString(font, "Mine", new Vector2(200, 100), Color.Black);
+
+            _spriteBatch.DrawString(font, mine.Minelvl, new Vector2(200, 120), Color.Black);
+
+            _spriteBatch.DrawString(font, mine.GoldHarvest, new Vector2(200, 140), Color.Black);
+
+            _spriteBatch.DrawString(font, "Upgrade", new Vector2(200, 160), Color.Black);
+            
+            _spriteBatch.DrawString(font, mine.BuildTime, new Vector2(200, 180), Color.Black);
+
+            _spriteBatch.DrawString(font, mine.UpgradeGoldHarvest, new Vector2(200, 200), Color.Black);
+
+            _spriteBatch.DrawString(font, mine.WoodCost, new Vector2(200, 220), Color.Black);
+
+            _spriteBatch.DrawString(font, mine.GoldCost, new Vector2(280, 220), Color.Black);
+
+            _spriteBatch.DrawString(font, mine.FoodCost, new Vector2(360, 220), Color.Black);
+
+            _spriteBatch.Draw(sprite, rectangle, Color.White);
+
 
             _spriteBatch.End();
 
             base.Draw(gameTime);
         }
+
+        
     }
 }
--- TownScape/Mine.cs
+++ TownScape/Mine.cs
@@ -5,20 +5,76 @@ using System.Threading;
 
 namespace TownScape
 {
+    delegate void UpgradeMineHandler(Mine mine);
     class Mine
     {
+        Sawmill sawmill = new Sawmill();
+        Field field = new Field();
+
+        public event UpgradeMineHandler UpgradeMine;
         public int goldPoints = 0;
         static readonly object lockObject = new object(); //used to lock shared data
 
         //booleans used to control looping of thread
         bool mineActive = true;
-        bool level1 = true;
+        public int level = 1;
+
+        string minelvl = "lvl 1";
+        string goldHarvest = "25 gold pr. 5 seconds";
+        string buildTime = "20 sec";
+        string upgradeGoldHarvest = "harvest 50 gold pr. 8 seconds";
+        string woodCost = "wood: 100";
+        string goldCost = "gold: 100";
+        string foodCost = "food: 50";
 
         public int GoldPoints
         {
             get { return goldPoints; }
             set { goldPoints = value; }
         }
+
+        public string Minelvl
+        {
+            get { return minelvl; }
+            set { minelvl = value; }
+        }
+
+        public string GoldHarvest
+        {
+            get { return goldHarvest; }
+            set { goldHarvest = value; }
+        }
+
+        public string BuildTime
+        {
+            get { return buildTime; }
+            set { buildTime = value; }
+        } 
+
+        public string UpgradeGoldHarvest
+        {
+            get { return upgradeGoldHarvest; }
+            set { upgradeGoldHarvest = value; }
+        }
+
+        public string WoodCost
+        {
+            get { return woodCost; }
+            set { woodCost = value; }
+        }
+
+        public string GoldCost
+        {
+            get { return goldCost; }
+            set { goldCost = value; }
+        }
+
+        public string FoodCost
+        {
+            get { return foodCost; }
+            set { foodCost = value; }
+        }
+
 
         public void TheMine()
         {
@@ -32,14 +88,72 @@ namespace TownScape
         {
             lock (lockObject)
             {
-                while (level1 == true && mineActive == true)
+                while (level == 1 && mineActive == true)
                 {
                     //1000 = 1 sec, 5000 = 5 sec, 30000 = 30 sec
-                    Thread.Sleep(2000);
+                    Thread.Sleep(5000);
 
                     goldPoints += 25;
                 }
-                    
+                minelvl = "lvl 2";
+                goldHarvest = "50 gold pr. 8 seconds";
+                buildTime = "40 sec";
+                upgradeGoldHarvest = "harvest 100 gold pr. 15 seconds";
+                woodCost = "wood: 150";
+                GoldCost = "gold: 200";
+                foodCost = "food: 125";
+
+                //building the upgrade
+                Thread.Sleep(20000);
+
+                while (level == 2 && mineActive == true)
+                {
+                    Thread.Sleep(8000);
+
+                    goldPoints += 50;
+                }
+                minelvl = "lvl 3";
+                goldHarvest = "100 gold pr. 15 seconds";
+                buildTime = "";
+                upgradeGoldHarvest = "";
+                woodCost = "";
+                GoldCost = "";
+                foodCost = "";
+
+                //building the upgrade
+                Thread.Sleep(40000);
+
+                while (level == 3 && mineActive == true)
+                {
+                    Thread.Sleep(15000);
+
+                    goldPoints += 100;
+                }
+
+            }
+        }
+
+        protected virtual void OnUpgradeMineEvent()
+        {
+            if (level == 1)
+            {
+                if (GoldPoints >= 100 && sawmill.WoodPoints >= 100 && field.FoodPoints >= 50)
+                {
+                    GoldPoints -= 100;
+                    sawmill.WoodPoints -= 100;
+                    field.FoodPoints -= 50;
+                    level++;
+                }
+            }
+            else if (level == 2)
+            {
+                if (GoldPoints >= 200 && sawmill.WoodPoints >= 150 && field.FoodPoints >= 125)
+                {
+                    GoldPoints -= 200;
+                    sawmill.WoodPoints -= 150;
+                    field.FoodPoints -= 125;
+                    level++;
+                }
             }
         }
     }
